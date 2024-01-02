f67f6b69a808c02be16a9c26b048e0e36e188578
Ben Eltschig <b.eltschig@protonmail.com>
2024-01-02 18:43:13 +0100

    made it so wrong combinations can no longer be selected, tweaked text size and made large tiles glow

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ queueMicrotask(()=>{
 			e.preventDefault();
 		});
 		tile.addEventListener("mouseenter",e=>{
-			if (selectedTiles.length&&selectedTiles[selectedTiles.length-1].neighbours.includes(tile)){
+			if (selectedTiles.length&&selectedTiles[selectedTiles.length-1].neighbours.includes(tile)&&selectedTiles[selectedTiles.length-1].value==tile.value){
 				selectedTiles.push(tile);
 				selectedTiles.splice(selectedTiles.indexOf(tile)+1).forEach(tile=>{tile.selected=false;});
 				tile.selected = true;
@@ -57,26 +57,25 @@ class HexagonalTile extends HTMLElement {
 				#tile {
 					width: 110%;
 					height: 110%;
-				}
-				#tile {
+					transition: background-color 0.05s ease;
 					background-color: #bfbfbf;
 					clip-path: polygon(${[0,1,2,3,4,5].map(i=>[0,1,2,3,4,5].map(j=>`${50+50*(0.925*Math.cos((i+0.5)*Math.PI/3)+0.075*Math.cos((i+(j-2.5)/5+0.5)*Math.PI/3))}% ${50+50*(0.925*Math.sin((i+0.5)*Math.PI/3)+0.075*Math.sin((i+(j-2.5)/5+0.5)*Math.PI/3))}%`)).flat().join(", ")});
 					pointer-events: auto;
 				}
-				:host([data-value]) #tile {
-					background: #cd9bff;
-				}
-				:host([data-value="1"]) #tile {
+				:host(.tile-1) #tile {
 					background: #cdebff;
 				}
-				:host([data-value="2"]) #tile {
+				:host(.tile-2) #tile {
 					background: #cdd7ff;
 				}
-				:host([data-value="3"]) #tile {
+				:host(.tile-3) #tile {
 					background: #cdc3ff;
 				}
-				:host([data-value="4"]) #tile {
+				:host(.tile-4) #tile, :host(.tile-5) #tile, :host(.tile-6) #tile, :host(.tile-7) #tile, :host(.tile-8) #tile, :host(.tile-9) #tile {
 					background: #cdafff;
+				}
+				:host(.tile-e1) #tile, :host(.tile-e2) #tile, :host(.tile-e3) #tile, :host(.tile-e4) #tile {
+					background: #cd9bff;
 				}
 				:host(.selected) #tile {
 					background-color: #ff0000 !important;
@@ -92,27 +91,64 @@ class HexagonalTile extends HTMLElement {
 					font-size: min(10vw,11.55vh);
 					pointer-events: none;
 				}
+				:host(.tile-e1) span {
+					font-size: min(9.5vw,10.973vh);
+				}
+				:host(.tile-e2) span {
+					font-size: min(7.5vw,8.663vh);
+				}
+				:host(.tile-e3) span {
+					font-size: min(6vw,6.93vh);
+				}
+				:host(.tile-e4) span {
+					font-size: min(5.2vw,6.006vh);
+				}
 				@media (prefers-color-scheme: dark) {
 					span {
 						color: white;
 					}
-					:host([data-value]) #tile {
-						background: #dfa020;
-					}
-					:host([data-value="1"]) #tile {
+					:host(.tile-1) #tile {
 						background: #383838;
 					}
-					:host([data-value="2"]) #tile {
+					:host(.tile-2) #tile {
 						background: #605c54;
 					}
-					:host([data-value="3"]) #tile {
+					:host(.tile-3) #tile {
 						background: #7c7064;
 					}
-					:host([data-value="4"]) #tile, :host([data-value="5"]) #tile {
+					:host(.tile-4) #tile, :host(.tile-5) #tile {
 						background: #a08050;
 					}
-					:host([data-value="6"]) #tile {
+					:host(.tile-6) #tile, :host(.tile-7) #tile, :host(.tile-8) #tile, :host(.tile-9) #tile {
 						background: #bf9040;
+					}
+					:host(.tile-e1) #tile {
+						background: #dfa020;
+					}
+					:host(.tile-e2) #tile {
+						background: #efa810;
+					}
+					:host(.tile-e3) #tile, :host(.tile-e4) #tile {
+						background: #ffaf00;
+					}
+					:host {
+						filter: drop-shadow(0 0 min(1vw,1.155vh) #ffcf6000) drop-shadow(0 0 min(5vw,5.775vh) #ffcf6000);
+						transition: filter 0.5s ease;
+					}
+					:host(.tile-e2) {
+						filter: drop-shadow(0 0 min(1vw,1.155vh) #ffcf6020) drop-shadow(0 0 min(5vw,5.775vh) #ffcf6040);
+						z-index: 1;
+					}
+					:host(.tile-e3) {
+						filter: drop-shadow(0 0 min(2vw,2.31vh) #ffcf6040) drop-shadow(0 0 min(10vw,11.55vh) #ffcf6080);
+						z-index: 2;
+					}
+					:host(.tile-e4) {
+						filter: drop-shadow(0 0 min(3vw,3.465vh) #ffcf6060) drop-shadow(0 0 min(15vw,17.325vh) #ffcf60c0);
+						z-index: 3;
+					}
+					:host(.selected) #tile {
+						background-color: #ff8000 !important;
 					}
 				}
 			</style>
@@ -135,7 +171,13 @@ class HexagonalTile extends HTMLElement {
 	set value(value){
 		this._value = value;
 		this._span.innerText = value;
-		this.setAttribute("data-value",value);
+		for (let i=1;i<10;i++) {
+			this.classList.toggle("tile-"+i,this._value==i);
+		}
+		this.classList.toggle("tile-e1",this._value.toString().length==2);
+		this.classList.toggle("tile-e2",this._value.toString().length==3);
+		this.classList.toggle("tile-e3",this._value.toString().length==4);
+		this.classList.toggle("tile-e4",this._value.toString().length>=5);
 	}
 
 	/** @readonly */
